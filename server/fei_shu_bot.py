7e2abab0d2bd10c078e0e802b413a2ba44c4dac4
Joey <zhangxiaodong@pshtec.com>
2024-08-30 10:24:54 +0800

    修复

--- server/fei_shu_bot.py
+++ server/fei_shu_bot.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from common.log import logger
 
 app = Flask(__name__)
 
@@ -6,6 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "hello, world"
+
+@app.route('/update-record', methods=["POST"])
+def update_record():
+    logger.info("收到更新请求".format(request))
 
 
 def run_server():
